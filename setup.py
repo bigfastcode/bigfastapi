2bc8d877991728b6af54d841d2ce1010a29f1cd4
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-12-03 09:00:48 +0100

    setup

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="BigFastAPI",
-    version="0.0.8",
+    name="bigfastapi",
+    version="0.0.1",
     author="Nwokoye Chigozie Gregory",
     author_email="gregoflash05@gmail.com.com",
     description="An Authentication Package built on Fast API",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rijentech/bigfastapi",
-    packages=["BigFastAPI"],
+    packages=["bigfastapi"],
     install_requires=[
         "aioredis",
         "aiosmtplib>=1.1.6",
