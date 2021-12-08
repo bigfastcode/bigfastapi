64433ada3acec3fd51d7cdd22947ad950ab57e86
Nwokoye Chigozie <gregoflash05@gmail.com>
2021-12-08 10:10:15 +0100

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bigfastapi",
-    version="0.0.1",
+    version="0.0.2",
     author="Nwokoye Chigozie Gregory",
     author_email="gregoflash05@gmail.com.com",
     description="An Authentication Package built on Fast API",
