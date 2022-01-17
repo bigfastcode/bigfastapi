a6b28d881166f1ec8a8dd0074cc2339f40bfe1bc
shadrach <oforkansi.shadrach@gmail.com>
2022-01-17 21:01:07 +0100

    countries flags modified

--- bigfastapi/countries.py
+++ bigfastapi/countries.py
@@ -5,6 +5,7 @@ import json
 
 app = APIRouter(tags=["Countries"])
 
+
 @app.get("/countries", response_model=_schemas.Country, status_code=200)
 def get_countries():
     """Get Countries and their respective states
@@ -21,12 +22,12 @@ def get_countries():
         for country in countries:
             del country["states"]
             del country["dial_code"]
-            del country['sample_phone_format']
+            del country["sample_phone_format"]
     return JSONResponse(status_code=status.HTTP_200_OK, content=countries)
 
 
-@app.get("/countries/{country}/states", response_model=_schemas.State, status_code=200)
-def get_country_states(country:str):
+@app.get("/countries/{country_code}/states", response_model=_schemas.State, status_code=200)
+def get_country_states(country_code: str):
     """Get states within a particular country
 
     Args:
@@ -38,16 +39,25 @@ def get_country_states(country:str):
     """
     with open("data/countries.json") as file:
         countries = json.load(file)
-        country_data = list(filter(lambda data: data["name"].casefold() == country.casefold(), countries))
+        country_data = list(
+            filter(
+                lambda data: data["iso2"].casefold() == country_code.casefold()
+                or data["iso3"].casefold() == country_code.casefold(),
+                countries,
+            )
+        )
         if country_data:
             data = country_data[0]
-            del data['dial_code']
-            del data['sample_phone_format']
+            del data["dial_code"]
+            del data["sample_phone_format"]
             return JSONResponse(status_code=status.HTTP_200_OK, content=data)
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Country not found")
-    
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Country not found"
+        )
+
+
 @app.get("/countries/codes", response_model=_schemas.Country, status_code=200)
-def get_countries_dial_codes(country:str = None):
+def get_countries_dial_codes(country_code: str = None):
     """Get Countries and their respective codes
         including dial codes and sample phone formats
 
@@ -60,22 +70,35 @@ def get_countries_dial_codes(country:str = None):
     """
     with open("data/countries.json") as file:
         countries = json.load(file)
-        if country:
-            country_search = list(filter(lambda data: data["name"].casefold() == country.casefold(), countries))
+        if country_code:
+            country_search = list(
+                filter(
+                lambda data: data["iso2"].casefold() == country_code.casefold()
+                or data["iso3"].casefold() == country_code.casefold(),
+                countries,
+            )
+        )
             country_found = country_search[0] if country_search != [] else {}
             if country_found:
                 if country_found["dial_code"] == "":
-                   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="dial code not found")
+                    raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND,
+                        detail="Country not found",
+                    )
                 del country_found["states"]
-                del country_found['flag_url']
-                del country_found['flag_unicode']
-                return JSONResponse(status_code=status.HTTP_200_OK, content=country_found)
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Country not found")
-        
+                del country_found["flag_url"]
+                del country_found["flag_unicode"]
+                return JSONResponse(
+                    status_code=status.HTTP_200_OK, content=country_found
+                )
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Country not found"
+            )
+
         for country_data in countries:
             if country_data["dial_code"] == "":
                 del country_data
             del country_data["states"]
-            del country_data['flag_url']
-            del country_data['flag_unicode']
+            del country_data["flag_url"]
+            del country_data["flag_unicode"]
         return JSONResponse(status_code=status.HTTP_200_OK, content=countries)
