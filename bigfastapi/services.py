eafb371ec9a1161ef57b79b4ee8670877b9c3bb8
TobeTek <katchyemma@gmail.com>
2022-01-18 15:05:37 +0100

    Update Typing Comment Services

--- bigfastapi/services.py
+++ bigfastapi/services.py
@@ -309,13 +309,13 @@ async def db_reply_to_comment(model_name:str, comment_id:int, comment: _schemas.
         return reply
     return None
 
-async def db_delete_comment(object_id: str|int, model_name:str, db: _orm.Session):
+async def db_delete_comment(object_id: int, model_name:str, db: _orm.Session):
     object = await db_retrieve_comment_by_id(object_id=object_id, model_name=model_name, db=db)
     db.delete(object)
     db.commit()
     return _schemas.Comment.from_orm(object)
     
-async def db_create_comment_for_object(object_id: int, comment: _schemas.CommentCreate, db: _orm.Session, model_name:str = None):
+async def db_create_comment_for_object(object_id: str, comment: _schemas.CommentCreate, db: _orm.Session, model_name:str = None):
     obj = _models.Comment(rel_id=object_id, model_name=model_name, text=comment.text, name=comment.name, email=comment.email)
     db.add(obj)
     db.commit()
