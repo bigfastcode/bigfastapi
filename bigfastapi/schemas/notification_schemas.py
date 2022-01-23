09992e6e5f88dace030dc539adaff91f02b64210
Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>
2022-01-23 16:20:14 +0100

    updated notification schema

--- bigfastapi/schemas/notification_schemas.py
+++ bigfastapi/schemas/notification_schemas.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 import pydantic as pydantic
-from pydantic import Field, EmailStr
+from pydantic import Field
 from typing import Optional, List
 from fastapi import Depends
 
 class NotificationBase(pydantic.BaseModel):
     content: str
-    recipient: EmailStr
+    recipient: str
     reference: str = Field(..., description="model instance reference eg. comment-9cd87677378946d88dc7903b6710ae55", max_length=100)
 
 class Notification(NotificationBase):
     id: str
-    creator: EmailStr
+    creator: str
     has_read: bool
     date_created: datetime
     last_updated: datetime
@@ -20,7 +20,7 @@ class Notification(NotificationBase):
         orm_mode = True
 
 class NotificationCreate(NotificationBase):
-    creator: EmailStr = Field(..., description="creator='' makes the authenticated user email the creator. If you want to override it, pass the email you want to use eg. creator='support@admin.com'")
+    creator: str = Field(..., description="creator='' makes the authenticated user email the creator. If you want to override it, pass the email you want to use eg. creator='support@admin.com'")
 
 class NotificationUpdate(NotificationBase):
     pass
