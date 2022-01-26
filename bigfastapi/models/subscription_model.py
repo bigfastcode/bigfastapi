7a489163aa99421846ee6b22515066f314803f1f
Eric Ogie Aghahowa <ericogia@yahoo.com>
2022-01-26 06:01:29 +0100

    Restructuring Sucscription To Share Relationship with Plan

--- bigfastapi/models/subscription_model.py
+++ bigfastapi/models/subscription_model.py
@@ -6,6 +6,7 @@ from tkinter.messagebox import CANCEL
 import sqlalchemy as _sql
 import sqlalchemy.orm as _orm
 from bigfastapi.models.organisation_models import Organization
+from bigfastapi.models.plan_model import Plan
 from bigfastapi.models.user_models import User
 import passlib.hash as _hash
 from sqlalchemy.schema import Column
@@ -19,20 +20,21 @@ from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
 import bigfastapi.db.database as _database
 
 
-# class Status(str, Enum):
-#     CANCELLED = 'cancelled'
-#     ACTIVE = 'active'
-#     SUSPENDED = 'suspended'
+class SubStatusEnum(str, Enum):
+    canceled = "canceled"
+    suspended = "suspended"
+    active = "active"
 
 
 class Subscription(_database.Base):
     __tablename__ = "subscription"
 
     id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
-    plan = Column(String(225), index=True)
-    # organization_id = Column(String(225), ForeignKey(Organization.id))
-    organization_id = Column(String(225), index=True)
+    organization_id = Column(String(225), ForeignKey(Organization.id))
+    plan_id = Column(String(225), ForeignKey(Plan.id))
     is_paid = Column(Boolean, default=True, index=True)
-    # status = Column(String(225), default='active', index=True)
+    active_status = Column(String(225), index=True,
+                           default=SubStatusEnum.active)
+
     date_created = Column(DateTime, default=_dt.datetime.utcnow)
     last_updated = Column(DateTime, default=_dt.datetime.utcnow)
