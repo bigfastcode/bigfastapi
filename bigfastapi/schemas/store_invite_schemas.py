9131750f80258c287c1ff8cc9ed1f83f9744cec0
devteni <fatunmbiteniola001@gmail.com>
2022-03-02 10:58:01 +0100

    updated the base schema for invites and created a new Invite schema

--- bigfastapi/schemas/store_invite_schemas.py
+++ bigfastapi/schemas/store_invite_schemas.py
@@ -1,18 +1,25 @@
 import datetime as dt
-
+from typing import Optional
 from .email_schema import Email
 import pydantic as _pydantic
 
 class _InviteBase(_pydantic.BaseModel):
     user_email: str
+    user_id: Optional[str]
+    user_role: Optional[str]
+    is_accepted: Optional[bool]
+    is_revoked: Optional[bool]
+    is_deleted: Optional[bool]
 
 class UserInvite(_InviteBase):
     store: dict
-    user_id: str
-    user_role: str
     app_url: str
     email_details: Email
 
+class Invite(_InviteBase):
+    store_id: str
+    invite_code: str
+    
     class Config:
         orm_mode = True
 
