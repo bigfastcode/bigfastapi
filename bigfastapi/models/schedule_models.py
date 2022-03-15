2a1d31f72c8625b39f5b1c42523cbd43aede593e
DisuToyin <disujt@gmail.com>
2022-03-15 15:32:45 +0100

    sch

--- bigfastapi/models/schedule_models.py
+++ bigfastapi/models/schedule_models.py
@@ -9,7 +9,7 @@ import bigfastapi.db.database as database
 
 
 class Schedule(database.Base):
-    __tablename__ = "schedule"
+    __tablename__ = "schedules"
     id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
     organization_id = Column(String(), ForeignKey("businesses.id"))
     # when can you start reminding a customer of a debt before due date
