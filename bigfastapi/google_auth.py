1fee41b77c46b789900812d201281273d784f222
DisuToyin <disujt@gmail.com>
2022-04-07 14:36:29 +0100

    debug

--- bigfastapi/google_auth.py
+++ bigfastapi/google_auth.py
@@ -72,10 +72,18 @@ async def login(request: Request):
 @app.get('/google/token')
 async def auth(request: Request, db: orm.Session = fastapi.Depends(get_db)):
 
+    print('ERICCCCCC')
+
     access_token = await oauth.google.authorize_access_token(request)
     print(access_token)
-    user_data = await oauth.google.parse_id_token(request, access_token)
+    user_data = {}
+    try:
+        user_data = await oauth.google.parse_id_token(request, access_token)
+    except:
+        return 'cant get user data'
+
     print(user_data)
+
     check_user = valid_email_from_db(user_data['email'], db)
 
     if check_user:
