f332e36157da03fd2266495334306e6ba2ae1881
devteni <fatunmbiteniola001@gmail.com>
2022-04-27 17:30:36 +0100

    feat: implemented pagination for fetch receipts endpoint

--- bigfastapi/schemas/receipt_schemas.py
+++ bigfastapi/schemas/receipt_schemas.py
@@ -3,11 +3,20 @@ from typing import Optional, List
 from pydantic import BaseModel,EmailStr
 
 
-class atrributes(BaseModel):
-    sender: EmailStr
+class Receipt(BaseModel):
+    id: Optional[str]
+    organization_id: Optional[str]
+    sender_email: Optional[str]
+    message: Optional[str]
+    subject: Optional[str]
+    recipient: Optional[str]
+    file_id: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+class atrributes(Receipt):
     recipient: List[EmailStr] = []
-    message: str
-    subject: str
 
 class ResponseModel(BaseModel):
     message: str
