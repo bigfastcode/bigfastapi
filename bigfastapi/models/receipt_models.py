24574b9ac85b10182a93b4a9540efdcdbf38b6cf
devteni <fatunmbiteniola001@gmail.com>
2022-05-06 13:53:23 +0100

    fix: changed message input type to Text on receipt model

--- bigfastapi/models/receipt_models.py
+++ bigfastapi/models/receipt_models.py
@@ -1,7 +1,7 @@
 import datetime as _dt
 from re import T
 from sqlalchemy.schema import Column
-from sqlalchemy.types import String, DateTime
+from sqlalchemy.types import String, DateTime, Text
 from sqlalchemy import ForeignKey
 from uuid import uuid4
 import bigfastapi.db.database as _database
@@ -11,7 +11,7 @@ class Receipt(_database.Base):
     id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
     organization_id = Column(String(255), index=True)
     sender_email = Column(String(255), index=True)
-    message = Column(String(255), index=True)
+    message = Column(Text, index=True)
     subject = Column(String(255), index=True)
     recipient = Column(String(255), index=True)
     file_id = Column(String(255), ForeignKey("files.id"))
