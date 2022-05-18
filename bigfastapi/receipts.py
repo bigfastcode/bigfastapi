6c080f9c416e4a34083dedab790dff2971b74e90
devteni <fatunmbiteniola001@gmail.com>
2022-05-18 18:34:43 +0100

    fix: fixed receipt sending and error handling

--- bigfastapi/receipts.py
+++ bigfastapi/receipts.py
@@ -1,17 +1,17 @@
 from base64 import encode
 import fastapi, os
-from fastapi import Depends, FastAPI, HTTPException, Request, status
+from fastapi import Depends, HTTPException, status
 from fastapi import APIRouter
 from fastapi import UploadFile, File
-from bigfastapi.models.receipt_models import Receipt, search_receipts, fetch_receipt_by_id
-from bigfastapi.models.organisation_models import Organization
-from bigfastapi.schemas import users_schemas
+from .email import send_email, send_receipt_email
+from .models.receipt_models import Receipt, search_receipts, fetch_receipt_by_id
+from .models.organisation_models import Organization
+from .schemas import users_schemas
 from .schemas import receipt_schemas
 from .schemas import pdf_schema
 from bigfastapi import pdfs
 from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
-import random
 from fastapi.encoders import jsonable_encoder
 from bigfastapi.db.database import get_db
 from .auth_api import is_authenticated
@@ -19,16 +19,19 @@ import sqlalchemy.orm as orm
 from sqlalchemy import and_
 from uuid import uuid4
 from fastapi import BackgroundTasks
-from bigfastapi.utils import paginator, settings
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from .utils import paginator, settings
 from typing import List, Optional
+
+from bigfastapi.models import organisation_models
+from .core import messages
+from .core.helpers import Helpers
+
 
 app = APIRouter(tags=["Receipts"])
 
 
-#send receipt endpoint
 @app.post("/receipts", status_code=201, response_model=receipt_schemas.SendReceiptResponse)
-def send_receipt(
+async def send_receipt(
     payload: receipt_schemas.atrributes, 
     background_tasks: BackgroundTasks, 
     db: orm.Session = Depends(get_db),
@@ -58,15 +61,26 @@ def send_receipt(
                 an object with a key `message` with a string value - `receipt sent` .
     """
     try: 
+        organization = await organisation_models.fetchOrganization(orgId=payload.organization_id, db=db)
+        if not organization:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                detail=messages.INVALID_ORGANIZATION)
+
+        is_valid_member = await Helpers.is_organization_member(user_id=user.id, organization_id=organization.id, db=db)
+        if is_valid_member == False:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=messages.NOT_ORGANIZATION_MEMBER)
 
         pdf_name = (payload.subject)+str(uuid4().hex)+".pdf"
         
-
         schema = {
                 "htmlString": payload.message,
                 "pdfName": pdf_name
         }
         
+        file = convert_to_pdf(pdf_schema.Format(**schema), db=db)
+
+        await send_receipt_email(payload, background_tasks=background_tasks, template="mail_receipt.html", db=db, file="./filestorage/pdfs/"+pdf_name)
+
         receipt = Receipt(
             id=uuid4().hex, 
             sender_email=payload.sender_email, 
@@ -76,16 +90,12 @@ def send_receipt(
             organization_id=payload.organization_id
             )
 
-        file = convert_to_pdf(pdf_schema.Format(**schema), db=db)
-
         receipt.file_id = file.id
         db.add(receipt)
         db.commit()
         db.refresh(receipt)
-        
-        send_receipt_email(payload, background_tasks=background_tasks, template="email/mail_receipt.html", db=db, file="./filestorage/pdfs/"+pdf_name)
 
-        return JSONResponse({"message" : "receipt sent"}, status_code=status.HTTP_201_CREATED)
+        return { "message" : "receipt sent" }
 
     except Exception as ex:
         if type(ex) == HTTPException:
@@ -124,6 +134,15 @@ async def fetch_receipts(
             returnBody- an object with a key `message` with a string value - `receipt sent` .
     """
     try:
+        organization = await organisation_models.fetchOrganization(orgId=organization_id, db=db)
+        if not organization:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                detail=messages.INVALID_ORGANIZATION)
+
+        is_valid_member = await Helpers.is_organization_member(user_id=user.id, organization_id=organization.id, db=db)
+        if is_valid_member == False:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=messages.NOT_ORGANIZATION_MEMBER)
+
         page_size = 50 if size < 1 or size > 100 else size
         page_number = 1 if page <= 0 else page
         offset = await paginator.off_set(page=page_number, size=page_size)
@@ -178,49 +197,25 @@ async def get_receipt(
 
             On sucessful request, it returns an object with the receipt details.
     """
-    receipt =  await fetch_receipt_by_id(receipt_id==receipt_id, org_id=organization_id, db=db)
-    return receipt
+    try: 
+        organization = await organisation_models.fetchOrganization(orgId=organization_id, db=db)
+        if not organization:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                detail=messages.INVALID_ORGANIZATION)
+
+        is_valid_member = await Helpers.is_organization_member(user_id=user.id, organization_id=organization.id, db=db)
+        if is_valid_member == False:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=messages.NOT_ORGANIZATION_MEMBER)
+
+        receipt =  await fetch_receipt_by_id(receipt_id==receipt_id, org_id=organization_id, db=db)
+        return receipt
+    except Exception as ex:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            , detail=str(ex))
 
 
 
 
-
-#SERVICES
-conf = ConnectionConfig(
-    MAIL_USERNAME=settings.MAIL_USERNAME,
-    MAIL_PASSWORD=settings.MAIL_PASSWORD,
-    MAIL_FROM=settings.MAIL_FROM,
-    MAIL_PORT=settings.MAIL_PORT,
-    MAIL_SERVER=settings.MAIL_SERVER,
-    MAIL_FROM_NAME=settings.MAIL_FROM_NAME,
-    MAIL_TLS=False,
-    MAIL_SSL=True,
-    USE_CREDENTIALS=True,
-    TEMPLATE_FOLDER=settings.TEMPLATE_FOLDER,
-)
-
-
-def send_receipt_email(
-    email_details: receipt_schemas.atrributes,
-    background_tasks: BackgroundTasks,
-    template: Optional[str] = "mail_receipt.html",
-    db: orm.Session = fastapi.Depends(get_db),
-    file: UploadFile = File(...)
-):
-
-    message = MessageSchema(
-        subject=email_details.subject,
-        recipients=email_details.recipient,
-        template_body={
-            "message": email_details.message,
-        },
-        subtype="html",
-        attachments=[file]
-        
-    )
-
-    fm = FastMail(conf)
-    background_tasks.add_task(fm.send_message, message, template_name=template)
 
 
 def convert_to_pdf(pdfSchema, db: orm.Session = Depends(get_db)):
