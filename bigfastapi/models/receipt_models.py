01f93984b730b5946e64e2b726dd153b116e9431
devteni <fatunmbiteniola001@gmail.com>
2022-05-20 11:49:54 +0100

    chore: refactored receipts

--- bigfastapi/models/receipt_models.py
+++ bigfastapi/models/receipt_models.py
@@ -25,34 +25,3 @@ class Receipt(_database.Base):
     file_id = Column(String(255), ForeignKey("files.id"))
     date_created = Column(DateTime, default=_dt.datetime.utcnow)
     last_updated = Column(DateTime, default=_dt.datetime.utcnow)
-
-
-
-async def search_receipts(
-    organization_id:str,
-    search_value: str,
-    offset: int, size:int=50,
-    db: orm.Session = Depends(get_db)
-    ):  
-    search_text = f"%{search_value}%"
-    search_result_count =db.query(Receipt).filter(and_(
-        Receipt.organization_id == organization_id,
-        Receipt.recipient.like(search_text))).count()
-
-    receipts_by_recipient = db.query(Receipt).filter(and_(
-        Receipt.organization_id == organization_id,
-        Receipt.recipient.like(search_text))
-        ).offset(
-        offset=offset).limit(limit=size).all()
-
-
-    recipient_list = [*receipts_by_recipient]
-    return (recipient_list, search_result_count)
-
-async def fetch_receipt_by_id(receipt_id:str, org_id: str, db: orm.Session = Depends(get_db)):
-    receipt = db.query(Receipt).filter(and_(
-             Receipt.id == receipt_id, Receipt.organization_id == org_id)
-            ).first()
-    if not receipt:
-        return {}
-    return receipt_schemas.Receipt.from_orm(receipt)
