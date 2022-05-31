be08ceb501cb725bb2e886b423e54a1e66a4bf13
devteni <fatunmbiteniola001@gmail.com>
2022-05-31 10:56:27 +0100

    bug fixes

--- bigfastapi/auth.py
+++ bigfastapi/auth.py
@@ -164,7 +164,7 @@ async def find_user_phone(phone_number, country_code, db: orm.Session):
 
 def send_slack_notification(user):
     message = "New login from " + user.email
-    await Helpers.slack_notification(text=message) # sends the message to slack
+    Helpers.slack_notification(text=message) # sends the message to slack
 
 
 
