befb25a4d968b9ad5875755b0cab2ece947c0d14
blacdev <stevenowoborode@gmail.com>
2022-06-27 08:57:31 +0100

    Fixed camel case use to snake case

--- bigfastapi/subscription.py
+++ bigfastapi/subscription.py
@@ -1,21 +1,20 @@
-from typing import List
-from bigfastapi import db
+from requests import Session
 from uuid import uuid4
 from bigfastapi.schemas import subscription_schema
+from bigfastapi.models import subscription_models
 from bigfastapi.db.database import get_db
-import sqlalchemy.orm as _orm
+from sqlalchemy.orm import Session
 import fastapi as _fastapi
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.param_functions import Depends
 from fastapi import APIRouter, HTTPException, status
-import fastapi
 
 app = APIRouter(tags=["Subscription"])
 
 
 @app.get("/subscriptions/{org_Id}", response_model=subscription_schema.ResponseList)
-async def indexSubPerOrg(org_Id: str,  db: _orm.Session = _fastapi.Depends(get_db)):
+async def index_sub_per_org(org_Id: str,  db: Session = _fastapi.Depends(get_db)):
     """intro-->This endpoint is used to retrieve a users subscription details to an organization. To use this endpoint you need to make a get request to the /subscriptions/{org_Id} endpoint 
             paramDesc-->On get request the url takes in the parameter, org_id
                 param-->org_id: This is the organization Id of the organization subscribed to
@@ -23,34 +22,34 @@ async def indexSubPerOrg(org_Id: str,  db: _orm.Session = _fastapi.Depends(get_d
     returnDesc--> On sucessful request, it returns 
         returnBody--> details of the subscription
     """
-    subs = await getSubs(org_Id, db)
-    return buildSuccessRess(list(map(subscription_schema.SubcriptionBase.from_orm, subs)),
+    subs = await get_subs(org_Id, db)
+    return build_success_ress(list(map(subscription_schema.SubcriptionBase.from_orm, subs)),
                             'subscription list', True)
 
 
 @app.post('/subscriptions', response_model=subscription_schema.ResponseSingle)
 async def subscribe(
         subscription: subscription_schema._SubBAse,
-        db: _orm.Session = _fastapi.Depends(get_db)
+        db: Session = _fastapi.Depends(get_db)
 ):
     """intro-->This endpoint is used to process subscription to an organization. To use this endpoint you need to make a post request to the /subscriptions endpoint with a specified body of request 
 
     returnDesc--> On sucessful request, it returns 
         returnBody--> details of the subscription
     """
-    createdSubscrcription = await createSub(subscription, db)
-    return buildSuccessRess(createdSubscrcription, 'subscription', False)
+    created_subscription = await create_subs(subscription, db)
+    return build_success_ress(created_subscription, 'subscription', False)
 
 # ///
 # SERVICE LAYER
 
 
-async def getSubs(org_Id: str, db: _orm.Session):
+async def get_subs(org_Id: str, db: Session):
     return db.query(subscription_models.Subscription).filter(
         subscription_models.Subscription.organization_id == org_Id).all()
 
 
-async def createSub(sub: subscription_schema.CreateSubscription, db: _orm.Session):
+async def create_subs(sub: subscription_schema.CreateSubscription, db: Session):
     subObject = subscription_models.Subscription(
         id=uuid4().hex, plan=sub.plan, organization_id=sub.organization_id, is_paid=True)
     db.add(subObject)
@@ -59,7 +58,7 @@ async def createSub(sub: subscription_schema.CreateSubscription, db: _orm.Sessio
     return subObject
 
 
-def buildSuccessRess(resData, type: str, isList: bool):
+def build_success_ress(resData, type: str, isList: bool):
     if isList:
         return subscription_schema.ResponseList(status='success', resource_type=type, data=resData)
     else:
