4f2a934dd7ef580da5e5188f41a8ace398babecb
blacdev <stevenowoborode@gmail.com>
2022-06-27 17:13:16 +0100

    Landing page table correection

--- bigfastapi/models/landing_page_models.py
+++ bigfastapi/models/landing_page_models.py
@@ -2,12 +2,11 @@
 from uuid import uuid4
 import datetime as datetime
 from sqlalchemy import ForeignKey
-from bigfastapi.bigfastapi.landing_page import landing_page_index
 import bigfastapi.db.database as db
-from sqlalchemy.orm import relationship
+import sqlalchemy.orm as orm
 from sqlalchemy.schema import Column, Table
-from sqlalchemy.types import String, DateTime, JSON, Boolean
-from sqlalchemy.sql import func
+from sqlalchemy.types import String, DateTime, JSON
+
 
 
 
@@ -17,19 +16,45 @@ class LPage(db.Base):
   id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
   user_id = Column(String(255), ForeignKey("users.id"))
   landing_page_name = Column(String(255), index=True, unique=True)
-  other_info = relationship("OtherInfo", back_populates="landing_page_data")
-  date_created = Column(DateTime, server_default=func.now())
-  last_updated = Column(DateTime, nullable=False,
-                        server_default=func.now(), onupdate=func.now())
+  content = Column('data', JSON)
+  date_created = Column(DateTime, default=datetime.datetime.utcnow)
+  last_updated = Column(DateTime, default=datetime.datetime.utcnow)
 
 
-class OtherInfo(db.Base):
-  __tablename__ = "landing_info"
-  id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
-  landing_page_id = Column(String(255), ForeignKey("landing_page"))
-  key = Column(String(255), index=True, default="")
-  value = Column(String(255), index=True, default="")
-  landing_page_data = relationship("LPage", back_populates="other_info")
-  date_created = Column(DateTime, server_default=func.now())
-  last_updated = Column(DateTime, nullable=False,
-                        server_default=func.now(), onupdate=func.now())
+
+
+
+
+# from uuid import uuid4
+# import datetime as datetime
+# from sqlalchemy import ForeignKey
+# import bigfastapi.db.database as db
+# from sqlalchemy.orm import relationship
+# from sqlalchemy.schema import Column, Table
+# from sqlalchemy.types import String, DateTime, JSON, Boolean
+# from sqlalchemy.sql import func
+
+
+
+
+# class LPage(db.Base):
+#   __tablename__ = 'landing_page'
+#   id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
+#   user_id = Column(String(255), ForeignKey("users.id"))
+#   landing_page_name = Column(String(255), index=True, unique=True)
+#   other_info = relationship("OtherInfo", back_populates="landing_page_data")
+#   date_created = Column(DateTime, server_default=func.now())
+#   last_updated = Column(DateTime, nullable=False,
+#                         server_default=func.now(), onupdate=func.now())
+
+
+# class OtherInfo(db.Base):
+#   __tablename__ = "landing_info"
+#   id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
+#   landing_page_id = Column(String(255), ForeignKey("landing_page"))
+#   key = Column(String(255), index=True, default="")
+#   value = Column(String(255), index=True, default="")
+#   landing_page_data = relationship("LPage", back_populates="other_info")
+#   date_created = Column(DateTime, server_default=func.now())
+#   last_updated = Column(DateTime, nullable=False,
+#                         server_default=func.now(), onupdate=func.now())
