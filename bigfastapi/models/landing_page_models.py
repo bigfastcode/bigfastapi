ba47ecfd644f0bdf059e392d374965172335c230
blacdev <stevenowoborode@gmail.com>
2022-06-29 12:48:37 +0100

    Fixed landing page models Modified landing page controllers created landing page services Deleted landing page template folder

--- bigfastapi/models/landing_page_models.py
+++ bigfastapi/models/landing_page_models.py
@@ -10,11 +10,12 @@ from sqlalchemy.sql import func
 
 
 
-class LPage(db.Base):
+class LandingPage(db.Base):
   __tablename__ = 'landing_page'
   id = Column(String(255), primary_key=True, index=True, default=uuid4().hex)
   user_id = Column(String(255), ForeignKey("users.id"))
   landing_page_name = Column(String(255), index=True, unique=True)
+  bucket_name = Column(String(255),)
   other_info = relationship("OtherInfo", back_populates="landing_page_data")
   date_created = Column(DateTime, server_default=func.now())
   last_updated = Column(DateTime, nullable=False,
@@ -27,7 +28,7 @@ class OtherInfo(db.Base):
   landing_page_id = Column(String(255), ForeignKey("landing_page.id"))
   key = Column(String(255), index=True, default="")
   value = Column(String(255), index=True, default="")
-  landing_page_data = relationship("LPage", back_populates="other_info")
+  landing_page_data = relationship("LandingPage", back_populates="other_info")
   date_created = Column(DateTime, server_default=func.now())
   last_updated = Column(DateTime, nullable=False,
                         server_default=func.now(), onupdate=func.now())
