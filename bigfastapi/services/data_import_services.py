f4ebea083df13ae777ca921f0f0a7daaff468a3a
blacdev <stevenowoborode@gmail.com>
2022-07-19 18:47:43 +0100

    Refactor: reconstructed os path to image

--- bigfastapi/services/data_import_services.py
+++ bigfastapi/services/data_import_services.py
@@ -66,43 +66,14 @@ def isEmpty(imports):
 
 
 
-"""
-This is to convert file to list  starting from the last line processed
-"""
-#return list of items after line processed in chunks
-def read_file_to_list(file:Any, line_processed: int = 1, file_for:str="Debt", field=None, is_length:bool=False):
-
-    file_length = list(csv.DictReader(file))
-
-
-    if is_length ==True:
-        return len(file_length)
-
-    if field != None:
-        fields = list(field)
-    #  check this urgently
-    
-    line_no =int(line_processed)
-    if line_no == 0:
-        line_no = 1
-    counter = itertools.count(line_no)
-    
-    if file_for == "Debt":
-        list_file_content=[dict(x, **{"line":next(counter)}) for x in file_length[int(line_processed):MAX_ROWS + 1]]
-
-    elif file_for == "customers":
-        list_file_content = [dict(x, **{"line":next(counter), "other_info":
-                            [{"key":i, "value":x[i]} for i in x.keys() if x not in fields]}) 
-                            for x in file_length[int(line_processed):MAX_ROWS +1]]
-    
-    return list_file_content
 
 """
 This is to get the file from the folder after querying the imports table
 """
 def retrieve_file(import_filename, bucket_name):
-    base_folder = os.path.realpath(FILES_BASE_FOLDER)
-    file = open(base_folder +"/" + bucket_name + "/"+import_filename, "rt")
+    folder = os.path.join(os.path.realpath(FILES_BASE_FOLDER), bucket_name, import_filename)
+    # base_folder = os.path.join(FILES_BASE_FOLDER)
+    file = open(folder, "rt")
     return file
 
 
