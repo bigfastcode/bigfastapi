ee17de9652f1612c709ba31791f4f0000c26f8e1
devteni <fatunmbiteniola001@gmail.com>
2022-08-23 14:25:17 +0100

    :page_facing_up: updated NOT_ORGANIZATION_MEMBER message

--- bigfastapi/core/messages.py
+++ bigfastapi/core/messages.py
@@ -1,3 +1,3 @@
-NOT_ORGANIZATION_MEMBER = "User not authorized to carry out this action"
+NOT_ORGANIZATION_MEMBER = "User not a member of this organization"
 INVALID_ORGANIZATION = "Organization does not exist"
 NON_UNIQUE_ID = "provided unique id already exist in this organization."
