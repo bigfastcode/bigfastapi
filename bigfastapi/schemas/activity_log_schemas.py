9d9c8368de416ff60bfa2f09084e33f1305cfaca
josephhensh <josephhensh2000@gmail.com>
2022-09-19 18:43:49 +0100

    Modify activity log schema to return only required details

--- bigfastapi/schemas/activity_log_schemas.py
+++ bigfastapi/schemas/activity_log_schemas.py
@@ -20,19 +20,29 @@ class DeleteActivitiesLogBase(pydantic.BaseModel):
         orm_mode = True
 
 
+# class ActivitiesLogOutput(pydantic.BaseModel):
+#     id : str
+#     action : str = '' 
+#     object_id : str
+#     object_url : str = ''
+#     model_name: str
+#     user_id: str
+#     organization_id: str
+#     organization : list = []
+#     user : list = []
+#     user_id : str
+#     is_deleted : bool
+#     created_at : datetime.datetime
+
+#     class Config:
+#         orm_mode = True
+
 class ActivitiesLogOutput(pydantic.BaseModel):
     id : str
-    action : str = '' 
-    object_id : str
-    object_url : str = ''
-    model_name: str
-    user_id: str
-    organization_id: str
-    organization : list = []
-    user : list = []
-    user_id : str
-    is_deleted : bool
+    action : str = ''     
+    model_name: str    
     created_at : datetime.datetime
 
     class Config:
         orm_mode = True
+
