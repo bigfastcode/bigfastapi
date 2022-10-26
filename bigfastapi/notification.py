a723a5d01545ef969b6df45f65e923038442dfbb
josephhensh <josephhensh2000@gmail.com>
2022-10-26 18:09:46 +0100

    Update file: add get, post endpoints for notifications; add get, post, put endpoints for notification settings

--- bigfastapi/notification.py
+++ bigfastapi/notification.py
@@ -1,14 +1,23 @@
 from fastapi import APIRouter
 from datetime import datetime
 from bigfastapi.db.database import get_db
-from fastapi import Depends
+from fastapi import Depends, status, HTTPException
 from .models import notification_models as model
 from .schemas import notification_schemas as schema, users_schemas as user_schema
 from typing import List
 from bigfastapi.services.auth_service import is_authenticated
 import sqlalchemy.orm as orm
 from uuid import uuid4
-
+from bigfastapi.models.organization_models import Organization
+from fastapi.responses import JSONResponse
+from .models.user_models import User
+from .services.notification_services import (
+    create_notification, 
+    create_notification_setting, 
+    fetch_existing_setting,
+    update_notification_setting
+)
+from bigfastapi.core.helpers import Helpers
 
 app = APIRouter(tags=["Notification"])
 
@@ -39,31 +48,178 @@ def get_all_notifications(db: orm.Session = Depends(get_db)):
     notifications = db.query(model.Notification).all()
     return list(map(schema.Notification.from_orm, notifications))
 
+# added
+@app.get("/notifications/{user_id}", response_model=List[schema.Notification])
+async def get_user_notifications(
+    user_id: str,
+    organization_id: str,
+    user: user_schema.User = Depends(is_authenticated),
+    db: orm.Session = Depends(get_db)):  
+
+    """intro-->This endpoint allows you to retrieve all notifications from the database. To retrieve you need to make a get request to the /notifications endpoint
+
+
+    returnDesc-->On sucessful request, it returns
+        returnBody--> an array of notifications.
+    """ 
+    #validate user and organization    
+    await Helpers.check_user_org_validity(
+            user_id=user.id, organization_id=organization_id, db=db
+        )     
+
+    notifications = db.query(model.Notification).join(model.NotificationRecipient).filter(
+                    model.NotificationRecipient.recipient_id == user_id 
+                    ).filter(model.NotificationRecipient.is_cleared == False).all()
+
+    #return notifications            
+    return list(map(schema.Notification.from_orm, notifications))
+    
+
+# @app.post("/notification", response_model=schema.Notification)
+# def create_notification(notification: schema.NotificationCreate, user: user_schema.User = Depends(is_authenticated),db: orm.Session = Depends(get_db)):
+
+#     """intro-->This endpoint allows you to create a new notification. To create, you need to make a post request to the /notification endpoint with a required body of request as specified below
+
+#         reqBody-->content: This is the content of the notification
+#         reqBody-->recipient: This the receiver of the notification
+#         reqBody-->reference: This is a unique identifier of the notification
+#         reqBody-->creator: This is the creator of the notification
+
+#     returnDesc-->On sucessful request, it returns
+#         returnBody--> the details of the newly created notification.
+#     """
+
+#     if notification.creator == "":
+#         creator = model.get_authenticated_user_email(user=user)
+#     else:
+#         creator = notification.creator
+
+#     new_notification = model.Notification(id=uuid4().hex, creator=creator, content=notification.content, reference=notification.reference, recipient=notification.recipient)
+#     db.add(new_notification)
+#     db.commit()
+#     db.refresh(new_notification)
+
+#     return schema.Notification.from_orm(new_notification)
+
+# added
 @app.post("/notification", response_model=schema.Notification)
-def create_notification(notification: schema.NotificationCreate, user: user_schema.User = Depends(is_authenticated),db: orm.Session = Depends(get_db)):
+async def create_user_notification(
+    notification: schema.NotificationCreate,
+    user: user_schema.User = Depends(is_authenticated),
+    db: orm.Session = Depends(get_db)):
 
     """intro-->This endpoint allows you to create a new notification. To create, you need to make a post request to the /notification endpoint with a required body of request as specified below
 
-        reqBody-->content: This is the content of the notification
+        reqBody-->message: This is the content of the notification
         reqBody-->recipient: This the receiver of the notification
-        reqBody-->reference: This is a unique identifier of the notification
         reqBody-->creator: This is the creator of the notification
 
     returnDesc-->On sucessful request, it returns
         returnBody--> the details of the newly created notification.
-    """
+    """     
+    #organization and user check
+    await Helpers.check_user_org_validity(
+            user_id=user.id, organization_id=notification.organization_id, db=db
+        )     
 
-    if notification.creator == "":
-        creator = model.get_authenticated_user_email(user=user)
-    else:
-        creator = notification.creator
+    new_notification = create_notification(notification=notification, user=user, db=db)          
 
-    new_notification = model.Notification(id=uuid4().hex, creator=creator, content=notification.content, reference=notification.reference, recipient=notification.recipient)
-    db.add(new_notification)
-    db.commit()
-    db.refresh(new_notification)
+    return new_notification
 
-    return schema.Notification.from_orm(new_notification)
+# added
+@app.get("/notifications-settings/{organization_id}", response_model=List[schema.NotificationSettingResponse])
+async def get_org_notification_settings(
+    organization_id: str,
+    user: user_schema.User = Depends(is_authenticated),
+    db: orm.Session = Depends(get_db)):  
+
+    """intro-->This endpoint allows you to retrieve all notifications from the database. To retrieve you need to make a get request to the /notifications endpoint
+
+
+    returnDesc-->On sucessful request, it returns
+        returnBody--> an array of notifications.
+    """ 
+    #validate user and organization    
+    await Helpers.check_user_org_validity(
+            user_id=user.id, organization_id=organization_id, db=db
+        )     
+
+    notification_settings = db.query(model.NotificationSetting).filter(
+                    model.NotificationSetting.organization_id == organization_id 
+                    ).all()
+
+    return notification_settings   
+
+    
+# added
+@app.post("/notification-settings", response_model=schema.NotificationSettingResponse)
+async def create_org_notification_settings(
+    notification_setting: schema.NotificationSetting,
+    user: user_schema.User = Depends(is_authenticated),
+    db: orm.Session = Depends(get_db)):
+
+    """intro-->This endpoint allows you to create a new notification. To create, you need to make a post request to the /notification endpoint with a required body of request as specified below
+
+        reqBody-->message: This is the content of the notification
+        reqBody-->recipient: This the receiver of the notification
+        reqBody-->creator: This is the creator of the notification
+
+    returnDesc-->On sucessful request, it returns
+        returnBody--> the details of the newly created notification.
+    """         
+    if user.is_superuser:
+
+        #organization and user check
+        await Helpers.check_user_org_validity(
+                user_id=user.id, organization_id=notification_setting.organization_id, db=db
+            )     
+
+        new_notification_setting = create_notification_setting(
+            notification_setting=notification_setting, 
+            user=user, 
+            db=db
+        )         
+        return new_notification_setting
+
+    return JSONResponse(
+            {"message": "Only an Admin can perform this action"}, 
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )    
+
+
+@app.put("/notification-settings/{setting_id}", response_model=schema.NotificationSettingResponse)
+async def update_org_notification_settings(
+    setting_id: str,
+    notification_setting: schema.NotificationSetting,
+    user: user_schema.User = Depends(is_authenticated),
+    db: orm.Session = Depends(get_db)):
+
+    """intro-->This endpoint allows you to create a new notification. To create, you need to make a post request to the /notification endpoint with a required body of request as specified below
+
+        reqBody-->message: This is the content of the notification
+        reqBody-->recipient: This the receiver of the notification
+        reqBody-->creator: This is the creator of the notification
+
+    returnDesc-->On sucessful request, it returns
+        returnBody--> the details of the newly created notification.
+    """     
+    #organization and user check
+    await Helpers.check_user_org_validity(
+            user_id=user.id, organization_id=notification_setting.organization_id, db=db
+        )     
+
+    existing_setting = await fetch_existing_setting(
+        id=setting_id,
+        organization_id=notification_setting.organization_id, 
+        db=db
+    )
+    updated_notification_setting = await update_notification_setting(
+        notification_setting=notification_setting, 
+        fetched_setting=existing_setting
+    )     
+    
+    return new_notification_setting
+
 
 @app.put("/notification/{notification_id}/read", response_model=schema.Notification)
 def mark_notification_read(notification_id: str,db: orm.Session = Depends(get_db)):  
