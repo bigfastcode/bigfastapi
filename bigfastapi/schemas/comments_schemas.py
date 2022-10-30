1e0cf210216ab00ffb4577990996b601483144f8
agughalamdavis <agughalamdavis@yahoo.com>
2022-10-30 11:46:40 +0100

    changes to comments schema

--- bigfastapi/schemas/comments_schemas.py
+++ bigfastapi/schemas/comments_schemas.py
@@ -7,30 +7,32 @@ from typing import List, Optional
 
 class CommentBase(pydantic.BaseModel):
     id: Optional[str]
-    text : str 
-    name : str
-    email : str
+    text: Optional[str]
+    name: Optional[str]
+    email: Optional[str]
     commenter_id: str = None
     org_id: Optional[str] = None
     date_created: Optional[datetime.datetime] = datetime.datetime.now()
     last_updated: Optional[datetime.datetime] = datetime.datetime.now()
-        
+
 
 class Comment(CommentBase):
-    id : str
-    rel_id : str
+    id: str
+    rel_id: str
     commenter_id: str = None
-    downvotes : int
-    upvotes : int
-    date_created : datetime.datetime
-    last_updated : datetime.datetime
-    replies : List["Comment"]
+    downvotes: int
+    upvotes: int
+    date_created: datetime.datetime
+    last_updated: datetime.datetime
+    replies: List["Comment"]
+
     class Config:
         orm_mode = True
+
 
 class CommentCreate(CommentBase):
     pass
 
+
 class CommentUpdate(CommentBase):
     pass
-        
