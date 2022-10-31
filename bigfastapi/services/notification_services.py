3bca7170e1a57b67fb718da8ae2fceed32c5d18f
josephhensh <josephhensh2000@gmail.com>
2022-10-31 02:31:42 +0100

    Update services: queries were broken down

--- bigfastapi/services/notification_services.py
+++ bigfastapi/services/notification_services.py
@@ -7,7 +7,9 @@ from bigfastapi.models.notification_models import(
     NotificationRecipient,
     NotificationGroupMember,
     NotificationGroupModule,
-    NotificationSetting as Setting
+    NotificationSetting as Setting,
+    NotificationModule,
+    NotificationGroup
 )
 from bigfastapi.models.organization_models import (
     OrganizationUser,
@@ -22,64 +24,127 @@ from bigfastapi.schemas import users_schemas as user_schema
 from uuid import uuid4
 
 
-def create_notification( #might later be an async def
+def create_notification( 
     notification: NotificationCreate,
     user: user_schema.User = Depends(is_authenticated),
     db: orm.Session = Depends(get_db)
 ):
-    new_notification = Notification(
-        id=uuid4().hex, 
-        creator_id=notification.creator_id, 
-        message=notification.message, 
-        access_level=notification.access_level, 
-    )
-    db.add(new_notification)
-    # db.commit()
-
-    recipient_ids = get_notification_recipients(
-        module=notification.module, 
-        organization_id=notification.organization_id, 
-        access_level=notification.access_level
-    )
-    for recipient in notification.recipient_ids:
-        new_notification_recipient = NotificationRecipient(
-            id=uuid4().hex, notification_id=new_notification.id, recipient_id=recipient
-        )
-        db.add(new_notification_recipient)
+    if type(notification) != dict:
+        notification = notification.dict()
+    # check if organization notification setting status is True
+    existing_setting = db.query(Setting).filter(
+                    Setting.organization_id == notification["organization_id"]
+                    ).first()
     
-    db.commit()
-    db.refresh(new_notification)    
+    if existing_setting.status == True:       
+        new_notification = Notification(
+            id=uuid4().hex, 
+            creator_id=notification["creator_id"], 
+            message=notification["message"], 
+            organization_id=notification["organization_id"],
+            access_level=notification["access_level"]         
+        )
+        db.add(new_notification)
+        # db.commit()
 
-    return new_notification
+        recipient_ids = get_notification_recipients(             
+            organization_id=notification["organization_id"],
+            module=notification["module"], 
+            access_level=notification["access_level"],            
+            db=db
+        )
+        
+        print(recipient_ids)
+        for recipient in recipient_ids:
+            new_notification_recipient = NotificationRecipient(
+                id=uuid4().hex, notification_id=new_notification.id, recipient_id=recipient, 
+                is_read=False, is_cleared=False
+            )
+            db.add(new_notification_recipient)
+        
+        db.commit()
+        db.refresh(new_notification)    
+
+        return new_notification
 
 
-def get_notification_recipients(module, organization_id, access_level):
-    group_members = db.query(NotificationGroupMember.member_id).join(NotificationGroupModule).filter(
-        NotificationGroupModule.module == module).all() 
+def get_notification_recipients(organization_id, module, access_level, db):
+    # Find module in notification_module table that matches module and get its id
+    notification_module = db.query(NotificationModule).filter(NotificationModule.module_name == module).first()
+    if notification_module != None:
+        notification_module_id = notification_module.id
 
+        # Get id of all groups associated with that module
+        notification_groups = db.query(NotificationGroupModule).filter(
+            NotificationGroupModule.module_id == notification_module_id).all()
+
+        notification_group_ids = []
+        for notification_group in notification_groups:
+            notification_group_ids.append(notification_group.group_id)            
+
+        # Get id of all group members in those groups
+        group_members = []
+
+        for group_id in notification_group_ids:
+            members = db.query(NotificationGroupMember).filter(
+                NotificationGroupMember.group_id == group_id).all()
+
+            group_member_ids = []
+            for group_member in members:
+                group_member_ids.append(group_member.member_id)           
+
+            for member_id in group_member_ids:
+                if member_id not in group_members:
+                    group_members.append(member_id)
+    else:
+        group_members = None                    
+    
     if access_level and group_members is None:
-        users = db.query(OrganizationUser.user_id).join(Role).filter(
+        users_id = []
+        users = db.query(OrganizationUser).join(Role).filter(
             OrganizationUser.organization_id == organization_id
             ).filter(Role.role_name == access_level).all()
-        return users
+        for user in users:
+            users_id.append(user.user_id)     
+        return users_id
 
     if access_level is None and group_members is None :
-        users = db.query(OrganizationUser.user_id).filter(
+        users_id = []
+        users = db.query(OrganizationUser).filter(
             OrganizationUser.organization_id == organization_id
             ).all()
-        return users
+        for user in users:
+            users_id.append(user.user_id)    
+        return users_id
 
     if group_members and access_level is None:
         return group_members
 
     if group_members and access_level:
-        #get group members with access_level = access_level    
-        pass                  
+
+        #get role_id with role = access_level
+        role = db.query(Role).filter(
+            Role.organization_id == organization_id
+            ).filter(Role.role_name == access_level).first()         
+        
+        role_id = role.id
+       
+        #get group members with access_level = access_level
+        group_members_with_access_level = []
+
+        for group_member in group_members:
+            member = db.query(OrganizationUser).filter(
+            OrganizationUser.organization_id == organization_id
+            ).filter(OrganizationUser.user_id == group_member).filter(
+            OrganizationUser.role_id == role_id).first()
+
+            if member != None: 
+                member_id = member.user_id
+                group_members_with_access_level.append(member_id)         
+       
+        return group_members_with_access_level                 
          
-
-    
-
-
+         
 def create_notification_setting(
     notification_setting: NotificationSetting, 
     user: user_schema.User = Depends(is_authenticated), 
@@ -92,6 +157,10 @@ def create_notification_setting(
         send_via=notification_setting.send_via
     )
 
+    db.add(new_settings)
+    db.commit()
+    db.refresh(new_settings) 
+    
     return new_settings
 
 
@@ -104,9 +173,11 @@ async def update_notification_setting(
     if notification_setting.access_level:
         fetched_setting.access_level = notification_setting.access_level
     if notification_setting.send_via:
-        fetched_setting.send_via = notification_setting.send_via   
+        fetched_setting.send_via = notification_setting.send_via
+    if notification_setting.status != "":
+        fetched_setting.status = notification_setting.status       
     fetched_setting.last_updated = notification_setting.last_updated if notification_setting.last_updated else datetime.now()
-    fetched_setting.last_updated_db = datetime.now()
+    
 
     db.commit()
     db.refresh(fetched_setting)
@@ -122,7 +193,7 @@ async def fetch_existing_setting(id:str, organization_id:str, db:orm.Session):
     return notification_setting
 
 
-async def get_organization_access_level(organization_id: str):
+async def get_organization_access_level(organization_id: str, db:orm.Session):
     org_notification_setting = db.query(Setting).filter(
         Setting.organization_id == organization_id 
     ).first()
