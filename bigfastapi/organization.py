1c4a9afab6d31a433929884e796c31396ee1eba8
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2022-11-08 10:30:44 +0100

    fetch role to return default global role

--- bigfastapi/organization.py
+++ bigfastapi/organization.py
@@ -16,7 +16,7 @@ from fastapi import (
 )
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from sqlalchemy import and_
+from sqlalchemy import and_, or_
 from sqlalchemy.sql import expression
 
 from bigfastapi.services.auth_service import is_authenticated
@@ -454,7 +454,10 @@ def get_roles(
 
         returnBody--> list of all available roles in the queried organization
     """
-    roles = db.query(Role).filter(Role.organization_id == organization_id)
+    roles = db.query(Role).filter(or_(
+        Role.organization_id == "-1",
+        Role.organization_id == organization_id
+    ))
     roles = list(map(organization_schemas.Role.from_orm, roles))
 
     return roles
