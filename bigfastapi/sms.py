d58dae25cd09a471e1ac38da6e49b891edab1ae1
DisuToyin <disujt@gmail.com>
2022-12-01 16:59:05 +0100

    implemented ability to send sms

--- bigfastapi/sms.py
+++ bigfastapi/sms.py
@@ -19,87 +19,47 @@ class ResponseModel(BaseModel):
     message: str
 
 
-class SendSMS():
-    providers: Dict[str, str] = {
-        "nuobject": "https://cloud.nuobjects.com/api/send"
+sms_api = "https://telex.im/api/send-message"
+
+ORGANIZATION_ID = settings.TELEX_ORGANIZATION_ID
+ORGANIZATION_KEY = settings.TELEX_ORGANIZATION_KEY
+
+@app.post("/sms/send")
+async def SendSMS(
+        sms_details: sms_schema.SMS,
+        # db: orm.Session = fastapi.Depends(get_db)
+    ):
+
+    """intro-->This endpoint allows you to send an sms. To use this endpoint you need to make a post request to the /sms/send endpoint
+
+        reqBody-->sender: This is the name of the sender
+        reqBody-->recipient: This is the recipient of the sms
+        reqBody-->body: This is the content of the sms
+        reqBody-->provider: This is the network provider
+        reqBody-->user: This the current user 
+        reqBody-->passkey: This is unique passkey
+    
+    returnDesc--> On sucessful request, it returns message,
+        returnBody--> "success"
+    """ 
+ 
+    data={
+        "sender": sms_details.sender,
+        "message_type": sms_details.message_type,
+        "customers": sms_details.recipient,
+        "content": sms_details.content         
     }
 
-    @app.post("/sms/send", response_model=ResponseModel)
-    async def send_sms(
-        sms_details: sms_schema.SMS,
-        db: orm.Session = fastapi.Depends(get_db)
-    ):
+    headers={
+        'Content-Type': 'application/json',
+        "ORGANIZATION-KEY":ORGANIZATION_KEY, 
+        "ORGANIZATION-ID": ORGANIZATION_ID
+    }
 
-        """intro-->This endpoint allows you to send an sms. To use this endpoint you need to make a post request to the /sms/send endpoint
+    req = requests.post(
+        url=sms_api, 
+        json=data,
+        headers=headers
+    )
 
-            reqBody-->sender: This is the name of the sender
-            reqBody-->recipient: This is the recipient of the sms
-            reqBody-->body: This is the content of the sms
-            reqBody-->provider: This is the network provider
-            reqBody-->user: This the current user 
-            reqBody-->passkey: This is unique passkey
-        
-        returnDesc--> On sucessful request, it returns message,
-            returnBody--> "success"
-        """ 
-        
-        print(sms_details)
-        if (sms_details.provider == "nuobject"):
-            req = requests.put(
-                SendSMS.providers.get("nuobject"),
-                params={
-                    "user": sms_details.user,
-                    "pass": sms_details.passkey,
-                    "from": sms_details.sender,
-                    "to": sms_details.recipient,
-                    "msg": sms_details.body
-                }
-            )
-            if(req.status_code == 200):
-                sms = sms_models.SMS(
-                    id=uuid4().hex,
-                    sender=sms_details.sender,
-                    recipient=sms_details.recipient,
-                    body=sms_details.body
-                )
-
-                db.add(sms)
-                db.commit()
-                db.refresh(sms)
-            return {"code": req.status_code, "message": req.text}
-
-        return {"message": "An error occured while sending sms"}
-
-    async def send_sms_reminder(
-        username: str, passkey: str, sender: str, recipient: str, body: str
-    ):
-
-        req = requests.put(
-            SendSMS.providers.get("nuobject"),
-            params={
-                "user": username,
-                "pass": passkey,
-                "from": sender,
-                "to": recipient,
-                "msg": body
-            }
-        )
-        return req
-
-
-    def sync_send_sms_reminder(
-        username: str, passkey: str, sender: str, recipient: str, body: str
-    ):
-
-        req = requests.put(
-            SendSMS.providers.get("nuobject"),
-            params={
-                "user": username,
-                "pass": passkey,
-                "from": sender,
-                "to": recipient,
-                "msg": body
-            }
-        )
-        return req
-
+    return json.loads(req.text)
