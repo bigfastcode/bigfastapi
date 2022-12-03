643d9a211c8e0ec53889a969ce766f9274efe703
josephhensh <josephhensh2000@gmail.com>
2022-12-03 22:32:58 +0100

    Update file: Add function that handles non-20X status code responses

--- bigfastapi/utils/response.py
+++ bigfastapi/utils/response.py
@@ -1,5 +1,6 @@
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+from fastapi import HTTPException
 
 class Response():
     def __init__(self, message, status_code, data={}):
@@ -9,16 +10,26 @@ class Response():
        
         
     def __call__(self):
-        # check if response code is a success code, then return success message
-        status = self.is_successful()
-        if status:
+        # check if response code is a success code, then return success message, else send error msg
+        
+        if self.is_successful():
             return JSONResponse(
                 {"message": self.message, "data": jsonable_encoder(self.data)}, 
                 status_code=self.status_code
             )
+        else:
+            self.send_error_msg()
+
 
     def is_successful(self):
         if self.status_code in [200, 201, 202]:
             return True
         else:
-            return False   
+            return False
+            
+
+    def send_error_msg(self):        
+        raise HTTPException(
+                    status_code=self.status_code,
+                    detail=self.message,
+                )        
